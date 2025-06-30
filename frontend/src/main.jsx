2f5169b4783fd98340fb403101076e7d5650f7f1
2006 <iki1000alti@gmail.com>
2025-06-30 22:26:37 +0300

    test: otomatik deploy kontrolu

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function App() {
-  return <h1>Frontend Başladı!</h1>;
+  return <h1>Frontend Otomatik Güncellendi!</h1>;
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />); 
